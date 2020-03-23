0f0b0c7fe9a3c90b3f696616f607e6b9709c9baf
itorr <itorrrrrr@me.com>
2020-03-23 19:48:21 +0800

    代码整理

--- nbnhhsh.user.js
+++ nbnhhsh.user.js
@@ -10,171 +10,182 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+((htmlText,cssText)=>{
 
-let APIURL = 'https://lab.magiconch.com/api/nbnhhsh/';
-let Nbnhhsh = {};
+	const APIURL = 'https://lab.magiconch.com/api/nbnhhsh/';
+	const Nbnhhsh = {};
+	let app;
 
-let guess = (text,onOver)=>{
-
-	if(Nbnhhsh[text]){
-		return onOver(Nbnhhsh[text]);
-	}
-	if(guess.x){
-		guess.x.abort();
-	}
-	app.loading = true;
-	guess.x = new XMLHttpRequest();
-	guess.x.open('POST',APIURL+'guess');
-	guess.x.setRequestHeader('content-type', 'application/json');
-	guess.x.withCredentials = true;
-	guess.x.onload = ()=>{
-		app.loading = false;
-		let data = JSON.parse(guess.x.responseText);
-
-		onOver(Nbnhhsh[text] = data);
+	const request = (method,url,data,onOver)=>{
+		let x = new XMLHttpRequest();
+		x.open(method,url);
+		x.setRequestHeader('content-type', 'application/json');
+		x.withCredentials = true;
+		x.onload = ()=> onOver(guess.x.responseText?JSON.parse(guess.x.responseText):guess.x.responseText);
+		x.send(JSON.stringify(data));
+		return x;
 	};
+	const guess = (text,onOver)=>{
+		text = text.trim();
 
-	guess.x.send(JSON.stringify({text}));
-};
-let submitTran = (name)=>{
+		if(Nbnhhsh[text]){
+			return onOver(Nbnhhsh[text]);
+		}
 
-	let text = prompt('输入缩写对应文字','');
-
-	if(!text || !text.trim || !text.trim()){
-		return;
-	}
-
-	let x = new XMLHttpRequest();
-	x.open('POST',APIURL+'translation/'+name);
-	x.setRequestHeader('content-type', 'application/json');
-	x.withCredentials = true;
-	x.onload = ()=>{
-		alert('感谢对好好说话项目的支持！审核通过后这条对应将会生效');
-	};
-	x.send(JSON.stringify({text}));
-};
-let nbnhhsh = ()=>{
-	let selection = window.getSelection();
-	let text = selection.toString().trim();
-
-	app.show = !!text && /[a-z0-9]/i.test(text)
-	// app.show = true;
-
-	if(app.show){
-		let rect = selection.getRangeAt(0).getBoundingClientRect();
-
-		let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-		let top  = Math.floor( scrollTop + rect.top +rect.height );
-		let left = Math.floor( rect.left );
-
-		app.top = top;
-		app.left = left;
-
-		guess(text,data=>{
-
-			if(data.error){
-				app.error = data.error;
-			}else{
-				app.error = null;
-				app.tags = data;
-				if(!data.length){
-					app.show = false;
-				}
-			}
+		if(guess.x){
+			guess.x.abort();
+		}
+		app.loading = true;
+		guess.x = request('POST',APIURL+'guess',{text},data=>{
+			Nbnhhsh[text] = data;
+			onOver(data);
+			app.loading = false;
 		});
+	};
+	const submitTran = name=>{
+		let text = prompt('输入缩写对应文字','');
 
-		timer();
-	}
-};
-let handle = ()=>{
-	setTimeout(nbnhhsh,1);
-};
+		if(!text || !text.trim || !text.trim()){
+			return;
+		}
 
-document.body.addEventListener('mouseup',handle);
-document.body.addEventListener('keyup',handle);
+		request('POST',APIURL+'translation/'+name,{text},()=>{
+			alert('感谢对好好说话项目的支持！审核通过后这条对应将会生效');
+		});
+	};
 
-let timer = ()=>{
-	let selection = window.getSelection();
-	let text = selection.toString().trim();
+	let lastText = null;
+	const timer = ()=>{
+		let selection = window.getSelection();
+		let text = selection.toString().trim();
 
-	if(!text){
-		app.show = false;
-	}else{
-		setTimeout(timer,300);
-	}
-};
+		let show = !!text && /[a-z0-9]/i.test(text);
+
+		if(show){
+			setTimeout(timer,300);
+		}else{
+			app.show = false;
+		}
+	};
+	const nbnhhsh = ()=>{
+		let selection = window.getSelection();
+		let text = selection.toString().trim();
+
+		app.show = !!text && /[a-z0-9]/i.test(text);
+
+		if(app.show){
+			let rect = selection.getRangeAt(0).getBoundingClientRect();
+
+			let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+			let top  = Math.floor( scrollTop + rect.top +rect.height );
+			let left = Math.floor( rect.left );
+
+			app.top = top;
+			app.left = left;
+
+			guess(text,data=>{
+				if(data.error){
+					app.error = data.error;
+				}else{
+					app.error = null;
+					app.tags = data;
+					if(!data.length){
+						app.show = false;
+					}
+				}
+			});
+			timer();
+		}
+	};
+	const handle = ()=>{
+		setTimeout(nbnhhsh,1);
+	};
 
 
-let el = document.createElement('div');
-document.body.appendChild(el);
+	const bodyEl = document.body;
+	
+	const createEl = html=>{
+		createEl.el.innerHTML = html;
+		return createEl.el.children[0];
+	};
+	createEl.el = document.createElement('div');
+
+	const appendToBodyEl = el=>{
+		bodyEl.appendChild(el);
+	};
+
+	const el = createEl(htmlText);
+
+	appendToBodyEl(createEl(`<style>${cssText}</style>`));
+
+	const loadJS = (src,onOver) =>{
+		let el = document.createElement('script');
+		el.src = src;
+		el.onload = onOver;
+		document.body.appendChild(el);
+	};
 
 
-el.innerHTML = `
-	<div class="nbnhhsh-box" v-if="show" :style="{top:top+'px',left:left+'px'}" @mousedown.prevent>
-		<div class="nbnhhsh-loading" v-if="loading">
-			加载中…
-		</div>
-		<div class="nbnhhsh-tag-list" v-else-if="tags.length">
-			<div class="nbnhhsh-tag-item" v-for="tag in tags">
-				<h4>{{tag.name}}</h4>
-				<div class="nbnhhsh-tran-list" v-if="tag.trans">
-					<span class="nbnhhsh-tran-item" v-for="tran in tag.trans">{{tran}}</span>
-				</div>
-				<div class="nbnhhsh-notran-box" v-else-if="tag.trans===null">
-					无对应文字
-				</div>
-				<div v-else-if="tag.inputting && tag.inputting.length !==0">
-					<div class="nbnhhsh-inputting-list">
-						<h5>有可能是</h5>
-						<span class="nbnhhsh-inputting-item" v-for="input in tag.inputting">{{input}}</span>
-					</div>
-				</div>
-				<div class="nbnhhsh-notran-box" v-else @click.prevent="submitTran(tag.name)">
-					尚未录入，我来提交对应文字
-				</div>
-				<a v-if="tag.trans!==null" @click.prevent="submitTran(tag.name)" class="nbnhhsh-add-btn" title="我来提交对应文字"></a>
+	const loadVUE = onOver =>{
+		if(window['Vue']){
+			onOver(window['Vue']);
+		}else{
+			loadJS('https://cdn.bootcss.com/vue/2.6.11/vue.min.js',()=>{
+				onOver(window['Vue']);
+			})
+		}
+	};
+
+	setTimeout(()=>{
+		loadVUE(Vue=>{
+			appendToBodyEl(el);
+			app = new Vue({
+				el,
+				data: {
+					tags:[],
+					loading:false,
+					show:false,
+					top:0,
+					left:0
+				},
+				methods:{
+					submitTran
+				}
+			});
+
+			bodyEl.addEventListener('mouseup',handle);
+			bodyEl.addEventListener('keyup',handle);
+		});
+	},800);
+})(`
+<div class="nbnhhsh-box" v-if="show" :style="{top:top+'px',left:left+'px'}" @mousedown.prevent>
+	<div class="nbnhhsh-loading" v-if="loading">
+		加载中…
+	</div>
+	<div class="nbnhhsh-tag-list" v-else-if="tags.length">
+		<div class="nbnhhsh-tag-item" v-for="tag in tags">
+			<h4>{{tag.name}}</h4>
+			<div class="nbnhhsh-tran-list" v-if="tag.trans">
+				<span class="nbnhhsh-tran-item" v-for="tran in tag.trans">{{tran}}</span>
 			</div>
+			<div class="nbnhhsh-notran-box" v-else-if="tag.trans===null">
+				无对应文字
+			</div>
+			<div v-else-if="tag.inputting && tag.inputting.length !==0">
+				<div class="nbnhhsh-inputting-list">
+					<h5>有可能是</h5>
+					<span class="nbnhhsh-inputting-item" v-for="input in tag.inputting">{{input}}</span>
+				</div>
+			</div>
+			<div class="nbnhhsh-notran-box" v-else @click.prevent="submitTran(tag.name)">
+				尚未录入，我来提交对应文字
+			</div>
+			<a v-if="tag.trans!==null" @click.prevent="submitTran(tag.name)" class="nbnhhsh-add-btn" title="我来提交对应文字"></a>
 		</div>
 	</div>
-`;
-
-
-let app;
-
-
-let j = (src,onOver) =>{
-	let el = document.createElement('script');
-	el.src = src;
-	if(onOver){
-		el.onload = onOver;
-	}
-	document.body.appendChild(el);
-};
-
-j('https://cdn.bootcss.com/vue/2.6.11/vue.min.js',()=>{
-	document.body.appendChild(el);
-	app = new Vue({
-		el,
-		data: {
-			tags:[],
-			loading:false,
-			show:false,
-			top:0,
-			left:0
-		},
-		methods:{
-			submitTran
-		}
-	})
-});
-
-
-let styleEl = document.createElement('style');
-document.body.appendChild(styleEl);
-
-styleEl.innerHTML=`
+</div>
+`, `
 .nbnhhsh-box{
 	position: absolute;
 	z-index:99999999999;
@@ -255,6 +266,4 @@ styleEl.innerHTML=`
 	color:#999;
 	padding:20px 0;
 }
-`;
-
-})();
+`);
